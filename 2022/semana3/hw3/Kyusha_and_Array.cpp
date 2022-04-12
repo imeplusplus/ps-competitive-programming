b474efa50cedd7c69bf51b5fe9dcaea8b21a425e
mateus sze cosenza <80402245+cosenza987@users.noreply.github.com>
2022-04-11 21:40:25 -0300

    Create Kyusha_and_Array.cpp

---
+++ 2022/semana3/hw3/Kyusha_and_Array.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/*
+
+Veja que para que a condição do problema ocorra, o menor número do vetor tem que dividir todos os números.
+
+*/
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    int n;
+    cin >> n;
+    vector<int> v(n);
+    int mn = INT_MAX;
+    for(int i = 0; i < n; i++) {
+        cin >> v[i];
+        mn = min(mn, v[i]);
+    }
+    bool pode = true;
+    for(int i = 0; i < n; i++) {
+        if(v[i] % mn != 0) {
+            pode = false;
+            break;
+        }
+    }
+    cout << (pode ? mn : -1) << "\n";
+    return 0;
+}
