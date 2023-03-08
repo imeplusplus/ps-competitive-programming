79dc7743bfe5ff2054497ccea4ddb938e82a6781
mateus sze cosenza <80402245+cosenza987@users.noreply.github.com>
2023-03-08 09:40:00 -0300

    Create c.cpp

---
+++ 2023/semana0/hw0/c.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+
+    //NÃO MISTURAR CIN/COUT COM SCANF/PRINTF
+
+    // cout << "Hello, World!\n";
+    printf("Hello, World!\n");
+}
