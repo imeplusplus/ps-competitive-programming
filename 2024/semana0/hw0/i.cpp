aefb806a052796528c4247fb072378cd7253f867
mateus sze cosenza <80402245+cosenza987@users.noreply.github.com>
2024-02-28 20:42:58 -0300

    Create i.cpp

---
+++ 2024/semana0/hw0/i.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int x;
+    // cin >> x;
+    // if(x == 0) cout << 1 << "\n";
+    // else cout << 0 << "\n";
+    scanf(" %d", &x);
+    if(x == 0) printf("1 \n");
+    else printf("0 \n");
+    return 0;
+}
